600dfe6e6e3eb3c24c5256426f52a3ded1df0b9a
gdrkbones <gdrkbones@gmail.com>
2023-01-20 09:02:59 -0500

    first

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': 'Hospital Managment',
+    'version': '1.0',
+    'summary': 'Hospital Managment Software',
+    'sequence': 10,
+    'description': """Hospital Managment Software""",
+    'category': 'Administration',
+    'website': 'https://www.odootest.com',
+    'depends': [],
+    'data': [
+        'security/ir.module.access.csv',
+        'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
